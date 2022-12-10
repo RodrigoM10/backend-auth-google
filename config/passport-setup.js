3d956520d983d6188a5b09c8c1396d7abb3fa78c
Rodrigo <rodrigomendozar10@gmail.com>
2022-12-10 20:55:23 -0300

    google-auth register

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -18,7 +18,7 @@ passport.use(
         {
             // options for google strategy
             clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLECLIENT_SECRET,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: '/auth/google/redirect',
         },
         (accessToken, refreshToken, profile, done) => {
